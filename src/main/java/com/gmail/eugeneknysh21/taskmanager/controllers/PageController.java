e57ec35483aa4ab57df66f66558d36c527f5685b
Eugene Knysh <eugeneknysh21@gmail.com>
2021-05-26 19:09:46 +0300

    Changing the controller method to display the page.

--- src/main/java/com/gmail/eugeneknysh21/taskmanager/controllers/PageController.java
+++ src/main/java/com/gmail/eugeneknysh21/taskmanager/controllers/PageController.java
@@ -2,11 +2,17 @@ package com.gmail.eugeneknysh21.taskmanager.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
     @GetMapping("/")
-    public String indexPage() {
+    public String getPage(@RequestParam(required = false) Integer y,
+                            @RequestParam(required = false) Integer m,
+                            @RequestParam(required = false) Integer d) {
+        if (y != null && m != null && d != null) {
+            return "day";
+        }
         return "index";
     }
 }
